041c0d2d157f19baa33d63404ee8c16691a68b14
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 16:56:34 +0200

    fix: debug poging 1 voor ReadGebruiker

--- src/readgebruiker.py
+++ src/readgebruiker.py
@@ -1,21 +1,17 @@
-class Gebruiker:
-    def __init__(self, gebruiker_id, naam):
-        self.gebruiker_id = gebruiker_id
-        self.naam = naam
-
 class GebruikerNotFoundException(Exception):
     pass
 
 class UserService:
-    def __init__(self):
+    def __init__(self, gebruiker_data):
+        # gebruiker_data is a list of dicts with keys: id, naam, email
         self._gebruikers = {}
-
-    def add_gebruiker(self, gebruiker_id, naam):
-        gebruiker = Gebruiker(gebruiker_id, naam)
-        self._gebruikers[gebruiker_id] = gebruiker
+        for g in gebruiker_data:
+            self._gebruikers[g["id"]] = dict(g)  # make a shallow copy
 
     def read_gebruiker(self, gebruiker_id):
-        if gebruiker_id in self._gebruikers:
-            return self._gebruikers[gebruiker_id]
-        else:
+        if not isinstance(gebruiker_id, int):
+            raise TypeError("gebruiker_id moet een integer zijn")
+        if gebruiker_id not in self._gebruikers:
             raise GebruikerNotFoundException(f"Gebruiker met id {gebruiker_id} niet gevonden.")
+        # Return a COPY, not the reference
+        return dict(self._gebruikers[gebruiker_id])
