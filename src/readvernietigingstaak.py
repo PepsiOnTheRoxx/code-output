39bb1b69b0170a64c9f2a7aeb7314a7b5b72ebda
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 17:54:05 +0200

    feat: eerste implementatiepoging ReadVernietigingstaak

---
+++ src/readvernietigingstaak.py
@@ -0,0 +1,36 @@
+from src.readvernietigingstaak_exceptions import *
+
+class VernietigingstaakService:
+    def get_vernietigingstaak_by_id(self, taak_id):
+        raise NotImplementedError
+
+    def read_vernietigingstaak(self, taak_id):
+        try:
+            taak = self.get_vernietigingstaak_by_id(taak_id)
+        except VernietigingstaakNotFoundException:
+            raise
+        except UnauthorizedAccessException:
+            raise
+
+        result = {}
+        try:
+            result["id"] = taak["id"]
+        except Exception:
+            raise VernietigingstaakInvalidAttributeException("id ontbreekt of ongeldig")
+        try:
+            result["aantekeningen"] = taak["aantekeningen"]
+        except Exception:
+            raise VernietigingstaakAantekeningenReadException
+        try:
+            result["datum"] = taak["datum"]
+        except Exception:
+            raise VernietigingstaakDatumReadException
+        try:
+            result["status"] = taak["status"]
+        except Exception:
+            raise VernietigingstaakStatusReadException
+
+        if result["status"] not in ["INGEPLAND", "UITGEVOERD", "GEANNULEERD"]:
+            raise VernietigingstaakStatusReadException
+
+        return result
