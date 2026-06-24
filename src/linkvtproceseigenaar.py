e67fbdfc450c8629c00aa96a795d36566b3017d5
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 17:58:50 +0200

    feat: eerste implementatiepoging LinkVTProceseigenaar

---
+++ src/linkvtproceseigenaar.py
@@ -0,0 +1,35 @@
+from src.linkvtproceseigenaar_exceptions import *
+
+class GebruikerNietGevondenException(Exception):
+    pass
+
+class TaakNietGevondenException(Exception):
+    pass
+
+class ProceseigenaarAlGekoppeldException(Exception):
+    pass
+
+class OngeldigeRelatieException(Exception):
+    pass
+
+class VernietigingstaakRelatiesService:
+    def __init__(self):
+        self._gebruikers = set([1, 2, 3, 4])
+        self._taken = set([100, 200, 300, 400])
+        self._koppelingen = set()
+
+    def koppel_proceseigenaar(self, gebruiker_id, vernietigingstaak_id):
+        if gebruiker_id is None or vernietigingstaak_id is None:
+            raise OngeldigeRelatieException()
+        if gebruiker_id not in self._gebruikers:
+            raise GebruikerNietGevondenException()
+        if vernietigingstaak_id not in self._taken:
+            raise TaakNietGevondenException()
+        key = (gebruiker_id, vernietigingstaak_id)
+        if key in self._koppelingen:
+            raise ProceseigenaarAlGekoppeldException()
+        self._koppelingen.add(key)
+        return True
+
+    def is_proceseigenaar(self, gebruiker_id, vernietigingstaak_id):
+        return (gebruiker_id, vernietigingstaak_id) in self._koppelingen
