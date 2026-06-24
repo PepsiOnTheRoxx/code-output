847db3242e4e2025e58140c8d5957da24dd172dc
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 17:01:26 +0200

    fix: debug poging 1 voor VTBehandelaarRelation

--- src/vtbehandelaarrelation.py
+++ src/vtbehandelaarrelation.py
@@ -1,60 +1,86 @@
-class Gebruiker:
-    def __init__(self, gebruiker_id, naam):
-        self.gebruiker_id = gebruiker_id
-        self.naam = naam
-
-class Vernietigingstaak:
-    def __init__(self, taak_id, beschrijving):
-        self.taak_id = taak_id
-        self.beschrijving = beschrijving
-
-class VTBehandelaarRelationError(Exception):
+class VTBehandelaarRelationException(Exception):
     pass
 
-class VTBehandelaarRelation:
+class Gebruiker:
+    def __init__(self, id, naam):
+        self.id = id
+        self.naam = naam
+
+    def __eq__(self, other):
+        return isinstance(other, Gebruiker) and self.id == other.id and self.naam == other.naam
+
+    def __hash__(self):
+        return hash((self.id, self.naam))
+
+class Vernietigingstaak:
+    def __init__(self, id, omschrijving):
+        self.id = id
+        self.omschrijving = omschrijving
+
+    def __eq__(self, other):
+        return isinstance(other, Vernietigingstaak) and self.id == other.id and self.omschrijving == other.omschrijving
+
+    def __hash__(self):
+        return hash((self.id, self.omschrijving))
+
+class DestructionTaskService:
     def __init__(self):
-        # Key: Vernietigingstaak.taak_id, Value: Gebruiker.gebruiker_id
-        self._relaties = dict()
-        # Houdt instanties bij zodat teruggegeven instanties origineel zijn
-        self._gebruikers = dict()
-        self._taken = dict()
+        # mapping: taak_id -> set van gebruikers (Gebruiker instanties)
+        self._taak_to_gebruikers = {}
+        # mapping: gebruiker_id -> set van taken (Vernietigingstaak instanties)
+        self._gebruiker_to_taken = {}
 
-    def koppel_behandelaar(self, gebruiker, vernietigingstaak):
+    def koppel_behandelaar(self, gebruiker, taak):
         if not isinstance(gebruiker, Gebruiker):
-            raise VTBehandelaarRelationError("Behandelaar is geen Gebruiker instantie")
-        if not isinstance(vernietigingstaak, Vernietigingstaak):
-            raise VTBehandelaarRelationError("Taak is geen Vernietigingstaak instantie")
-        if vernietigingstaak.taak_id in self._relaties:
-            raise VTBehandelaarRelationError("Taak heeft al een behandelaar")
-        self._relaties[vernietigingstaak.taak_id] = gebruiker.gebruiker_id
-        self._gebruikers[gebruiker.gebruiker_id] = gebruiker
-        self._taken[vernietigingstaak.taak_id] = vernietigingstaak
+            raise VTBehandelaarRelationException("Behandelaar is geen Gebruiker instantie")
+        if not isinstance(taak, Vernietigingstaak):
+            raise VTBehandelaarRelationException("Taak is geen Vernietigingstaak instantie")
 
-    def ontkoppel_behandelaar(self, vernietigingstaak):
-        if not isinstance(vernietigingstaak, Vernietigingstaak):
-            raise VTBehandelaarRelationError("Taak is geen Vernietigingstaak instantie")
-        taak_id = vernietigingstaak.taak_id
-        if taak_id not in self._relaties:
-            raise VTBehandelaarRelationError("Taak heeft geen behandelaar gekoppeld")
-        del self._relaties[taak_id]
-        # Schoon verder netjes op als taak niet meer bestaat
-        del self._taken[taak_id]
+        if taak.id not in self._taak_to_gebruikers:
+            self._taak_to_gebruikers[taak.id] = set()
+        # Check of gebruiker al gekoppeld is aan deze taak
+        if gebruiker in self._taak_to_gebruikers[taak.id]:
+            raise VTBehandelaarRelationException("Deze behandelaar is al gekoppeld aan deze taak")
+        self._taak_to_gebruikers[taak.id].add(gebruiker)
 
-    def get_behandelaar(self, vernietigingstaak):
-        if not isinstance(vernietigingstaak, Vernietigingstaak):
-            raise VTBehandelaarRelationError("Taak is geen Vernietigingstaak instantie")
-        taak_id = vernietigingstaak.taak_id
-        if taak_id not in self._relaties:
-            return None
-        gebruiker_id = self._relaties[taak_id]
-        gebruiker = self._gebruikers.get(gebruiker_id)
-        return gebruiker
+        if gebruiker.id not in self._gebruiker_to_taken:
+            self._gebruiker_to_taken[gebruiker.id] = set()
+        self._gebruiker_to_taken[gebruiker.id].add(taak)
+        return True
 
-    def get_taak(self, gebruiker):
+    def ontkoppel_behandelaar(self, gebruiker, taak):
         if not isinstance(gebruiker, Gebruiker):
-            raise VTBehandelaarRelationError("Gebruiker is geen Gebruiker instantie")
-        gebruikers_id = gebruiker.gebruiker_id
-        for taak_id, gebruiker_id in self._relaties.items():
-            if gebruiker_id == gebruikers_id:
-                return self._taken[taak_id]
-        return None
+            raise VTBehandelaarRelationException("Behandelaar is geen Gebruiker instantie")
+        if not isinstance(taak, Vernietigingstaak):
+            raise VTBehandelaarRelationException("Taak is geen Vernietigingstaak instantie")
+
+        if taak.id not in self._taak_to_gebruikers or gebruiker not in self._taak_to_gebruikers[taak.id]:
+            raise VTBehandelaarRelationException("Behandelaar is niet gekoppeld aan deze taak")
+        self._taak_to_gebruikers[taak.id].remove(gebruiker)
+        if not self._taak_to_gebruikers[taak.id]:
+            del self._taak_to_gebruikers[taak.id]
+        self._gebruiker_to_taken[gebruiker.id].remove(taak)
+        if not self._gebruiker_to_taken[gebruiker.id]:
+            del self._gebruiker_to_taken[gebruiker.id]
+        return True
+
+    def is_behandelaar(self, gebruiker, taak):
+        if not isinstance(gebruiker, Gebruiker):
+            return False
+        if not isinstance(taak, Vernietigingstaak):
+            return False
+        return taak.id in self._taak_to_gebruikers and gebruiker in self._taak_to_gebruikers[taak.id]
+
+    def get_behandelaars(self, taak):
+        if not isinstance(taak, Vernietigingstaak):
+            raise VTBehandelaarRelationException("Taak is geen Vernietigingstaak instantie")
+        if taak.id in self._taak_to_gebruikers:
+            return set(self._taak_to_gebruikers[taak.id])
+        return set()
+
+    def get_taken(self, gebruiker):
+        if not isinstance(gebruiker, Gebruiker):
+            raise VTBehandelaarRelationException("Behandelaar is geen Gebruiker instantie")
+        if gebruiker.id in self._gebruiker_to_taken:
+            return set(self._gebruiker_to_taken[gebruiker.id])
+        return set()
