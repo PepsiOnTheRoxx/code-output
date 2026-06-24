82eb9113376f4a91c646b87ef56542aa7cee56ae
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 20:05:35 +0200

    feat: eerste implementatiepoging ManageVTBronnenRelatie

---
+++ src/services/managevtbronnenrelatie.py
@@ -0,0 +1,47 @@
+from src.services.managevtbronnenrelatie_exceptions import (
+    VTBronnenRelatieNotFoundException,
+    VTBronAlreadyLinkedException,
+    VTBronnenRelatieUpdateException
+)
+
+class VTBronnenRelatieRepository:
+    def exists(self, vernietigingstaak_id, bron_id):
+        # Implementatie van de persistence laag. Wordt gemocked in tests.
+        raise NotImplementedError
+
+    def add(self, vernietigingstaak_id, bron_id):
+        raise NotImplementedError
+
+    def delete(self, vernietigingstaak_id, bron_id):
+        raise NotImplementedError
+
+    def update(self, vernietigingstaak_id, bron_id, nieuwe_bron_id):
+        raise NotImplementedError
+
+    def get_bronnen_by_taak(self, vernietigingstaak_id):
+        raise NotImplementedError
+
+class VTBronnenRelatieService:
+    def __init__(self):
+        self._repo = VTBronnenRelatieRepository()
+
+    def create_relatie(self, vernietigingstaak_id, bron_id):
+        if self._repo.exists(vernietigingstaak_id=vernietigingstaak_id, bron_id=bron_id):
+            raise VTBronAlreadyLinkedException()
+        self._repo.add(vernietigingstaak_id=vernietigingstaak_id, bron_id=bron_id)
+        return True
+
+    def delete_relatie(self, vernietigingstaak_id, bron_id):
+        if not self._repo.exists(vernietigingstaak_id=vernietigingstaak_id, bron_id=bron_id):
+            raise VTBronnenRelatieNotFoundException()
+        self._repo.delete(vernietigingstaak_id=vernietigingstaak_id, bron_id=bron_id)
+        return True
+
+    def update_relatie(self, vernietigingstaak_id, bron_id, nieuwe_bron_id):
+        if not self._repo.exists(vernietigingstaak_id=vernietigingstaak_id, bron_id=bron_id):
+            raise VTBronnenRelatieNotFoundException()
+        self._repo.update(vernietigingstaak_id=vernietigingstaak_id, bron_id=bron_id, nieuwe_bron_id=nieuwe_bron_id)
+        return True
+
+    def get_bronnen_by_taak(self, vernietigingstaak_id):
+        return self._repo.get_bronnen_by_taak(vernietigingstaak_id=vernietigingstaak_id)
