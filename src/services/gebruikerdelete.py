9e5f1e8c571e2c8aaee00e919561d5969509e671
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 22:41:37 +0200

    feat: eerste implementatiepoging GebruikerDelete

---
+++ src/services/gebruikerdelete.py
@@ -0,0 +1,26 @@
+from src.services.gebruikerdelete_exceptions import (
+    GebruikerDeleteException,
+    GebruikerNietGevondenException as _OrigNotFoundExc,
+    GebruikerDeleteNietToegestaanException,
+    GebruikerDeleteDatabaseFoutException,
+    GebruikerDeleteOnverwachteFoutException
+)
+
+class GebruikerNotFoundException(_OrigNotFoundExc):
+    pass
+
+class GebruikerService:
+    def verwijder_gebruiker(self, gebruiker_id):
+        if not isinstance(gebruiker_id, int):
+            raise TypeError("gebruiker_id moet een integer zijn")
+
+        gebruiker = self._vind_gebruiker(gebruiker_id)
+        if gebruiker is None:
+            raise GebruikerNotFoundException(f'Gebruiker met id {gebruiker_id} niet gevonden')
+        self._verwijder_gebruiker(gebruiker)
+
+    def _vind_gebruiker(self, gebruiker_id):
+        raise NotImplementedError
+
+    def _verwijder_gebruiker(self, gebruiker):
+        raise NotImplementedError
