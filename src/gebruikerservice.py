8016293f42048dc31111e450307c559ae027fe66
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 13:16:13 +0200

    feat: implementatie van Update Gebruiker

--- src/gebruikerservice.py
+++ src/gebruikerservice.py
@@ -1,14 +1,23 @@
-class GebruikerNotFoundException(Exception):
-    pass
+class Gebruiker:
+    def __init__(self, id, naam, email):
+        self.id = id
+        self.naam = naam
+        self.email = email
 
 class GebruikerService:
     def __init__(self):
         self._gebruikers = {}
 
-    def add_gebruiker(self, gebruiker_id, naam, emailadres):
-        self._gebruikers[gebruiker_id] = {"Naam": naam, "Emailadres": emailadres}
+    def add_gebruiker(self, gebruiker):
+        self._gebruikers[gebruiker.id] = gebruiker
 
-    def read_gebruiker(self, gebruiker_id):
+    def update_gebruiker(self, gebruiker_id, updated_data):
         if gebruiker_id not in self._gebruikers:
-            raise GebruikerNotFoundException()
-        return self._gebruikers[gebruiker_id]
+            raise KeyError("Gebruiker niet gevonden")
+        gebruiker = self._gebruikers[gebruiker_id]
+        allowed_attrs = ("naam", "email")
+        for attr in updated_data:
+            if attr not in allowed_attrs:
+                raise AttributeError(f"Attribute '{attr}' mag niet gewijzigd worden")
+            setattr(gebruiker, attr, updated_data[attr])
+        return gebruiker
