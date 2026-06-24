15aca028a84c4e0f0a18e9da757847efca951219
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 13:16:27 +0200

    feat: implementatie van Delete Gebruiker

--- src/gebruikerservice.py
+++ src/gebruikerservice.py
@@ -1,23 +1,20 @@
-class Gebruiker:
-    def __init__(self, id, naam, email):
-        self.id = id
-        self.naam = naam
-        self.email = email
+class GebruikerNietGevondenFout(Exception):
+    pass
 
 class GebruikerService:
     def __init__(self):
         self._gebruikers = {}
 
-    def add_gebruiker(self, gebruiker):
-        self._gebruikers[gebruiker.id] = gebruiker
+    def voeg_gebruiker_toe(self, gebruiker_id, naam, email):
+        self._gebruikers[gebruiker_id] = {
+            "naam": naam,
+            "email": email
+        }
 
-    def update_gebruiker(self, gebruiker_id, updated_data):
-        if gebruiker_id not in self._gebruikers:
-            raise KeyError("Gebruiker niet gevonden")
-        gebruiker = self._gebruikers[gebruiker_id]
-        allowed_attrs = ("naam", "email")
-        for attr in updated_data:
-            if attr not in allowed_attrs:
-                raise AttributeError(f"Attribute '{attr}' mag niet gewijzigd worden")
-            setattr(gebruiker, attr, updated_data[attr])
-        return gebruiker
+    def verwijder_gebruiker(self, gebruiker_id):
+        if not gebruiker_id or gebruiker_id not in self._gebruikers:
+            raise GebruikerNietGevondenFout()
+        del self._gebruikers[gebruiker_id]
+
+    def bestaat_gebruiker(self, gebruiker_id):
+        return gebruiker_id in self._gebruikers
