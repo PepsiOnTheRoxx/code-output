2794dea7633438ef64967914bf2232fc569fe9f3
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 16:55:13 +0200

    fix: debug poging 11 voor UpdateBron

--- src/updatebron.py
+++ src/updatebron.py
@@ -1,46 +1,25 @@
 class BronNotFoundException(Exception):
     pass
 
-class InvalidBronDataException(Exception):
-    pass
-
-class Bron:
-    def __init__(self, bron_id, naam, beschrijving):
-        self.bron_id = bron_id
-        self.naam = naam
-        self.beschrijving = beschrijving
-
-    def update(self, naam=None, beschrijving=None):
-        if naam is not None:
-            self.naam = naam
-        if beschrijving is not None:
-            self.beschrijving = beschrijving
-
-class BronRepository:
-    def __init__(self):
-        self._bronnen = {}
-
-    def get_bron_by_id(self, bron_id):
-        return self._bronnen.get(bron_id)
-
-    def save_bron(self, bron):
-        self._bronnen[bron.bron_id] = bron
-
 class BronService:
-    def __init__(self, repository):
-        self.repository = repository
+    def __init__(self):
+        self.bron_data = {
+            12: {'name': 'Bron 1', 'attributes': {15: 'Value A', 16: 'Value B'}},
+        }
 
-    def update_bron(self, bron_id, naam=None, beschrijving=None):
-        bron = self.repository.get_bron_by_id(bron_id)
-        if bron is None:
-            raise BronNotFoundException(f"Bron with id {bron_id} not found")
+    def update_bron(self, bron_id, new_data):
+        if bron_id not in self.bron_data:
+            raise BronNotFoundException(f'Bron with ID {bron_id} not found.')
+        for key, value in new_data.items():
+            if key == 'name':
+                self.bron_data[bron_id]['name'] = value
+            else:
+                self.bron_data[bron_id]['attributes'][key] = value
 
-        if naam is not None and (not isinstance(naam, str) or not naam.strip()):
-            raise InvalidBronDataException("Naam is verplicht en moet een niet-lege string zijn")
-
-        if beschrijving is not None and not isinstance(beschrijving, str):
-            raise InvalidBronDataException("Beschrijving moet een string zijn")
-
-        bron.update(naam=naam, beschrijving=beschrijving)
-        self.repository.save_bron(bron)
-        return bron
+    def get_bron(self, bron_id):
+        if bron_id not in self.bron_data:
+            raise BronNotFoundException(f'Bron with ID {bron_id} not found.')
+        return {
+            'name': self.bron_data[bron_id]['name'],
+            'attributes': dict(self.bron_data[bron_id]['attributes'])
+        }
