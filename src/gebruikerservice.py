9b6b295fe9fd10bbb49146b15d23468a11fe302c
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 13:15:57 +0200

    feat: implementatie van Read Gebruiker

--- src/gebruikerservice.py
+++ src/gebruikerservice.py
@@ -1,32 +1,14 @@
-class Gebruiker:
-    _id_counter = 1
-
-    def __init__(self, emailadres, naam):
-        self.id = Gebruiker._id_counter
-        Gebruiker._id_counter += 1
-        self.emailadres = emailadres
-        self.naam = naam
+class GebruikerNotFoundException(Exception):
+    pass
 
 class GebruikerService:
     def __init__(self):
         self._gebruikers = {}
 
-    def create_gebruiker(self, emailadres, naam):
-        if emailadres is None or emailadres == '':
-            raise ValueError("Emailadres ontbreekt")
-        if naam is None or naam == '':
-            raise ValueError("Naam ontbreekt")
-        if not self._valid_email(emailadres):
-            raise ValueError("Ongeldig e-mail formaat")
-        if emailadres in self._gebruikers:
-            raise ValueError("Emailadres bestaat al")
-        gebruiker = Gebruiker(emailadres, naam)
-        self._gebruikers[emailadres] = gebruiker
-        return gebruiker
+    def add_gebruiker(self, gebruiker_id, naam, emailadres):
+        self._gebruikers[gebruiker_id] = {"Naam": naam, "Emailadres": emailadres}
 
-    def _valid_email(self, email):
-        if not isinstance(email, str):
-            return False
-        if "@" not in email or "." not in email.split("@")[-1]:
-            return False
-        return True
+    def read_gebruiker(self, gebruiker_id):
+        if gebruiker_id not in self._gebruikers:
+            raise GebruikerNotFoundException()
+        return self._gebruikers[gebruiker_id]
