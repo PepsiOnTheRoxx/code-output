169e22d6b73252dc525ae99faa3eb4d2f234e993
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 17:45:31 +0200

    feat: eerste implementatiepoging CreateGebruiker

---
+++ src/creategebruiker.py
@@ -0,0 +1,35 @@
+from src.creategebruiker_exceptions import *
+
+import re
+
+class Gebruiker:
+    def __init__(self, naam, email):
+        self.naam = naam
+        self.email = email
+
+class GebruikerService:
+    def __init__(self):
+        self._gebruikers = []
+
+    def create_gebruiker(self, naam, email):
+        if not naam:
+            raise ValueError("Naam mag niet leeg zijn")
+        if not email:
+            raise ValueError("Email mag niet leeg zijn")
+        if not self._is_geldig_email(email):
+            # Exception class has different names in exceptions vs tests
+            try:
+                exception_class = OngeldigEmailadresException
+            except NameError:
+                exception_class = GebruikerEmailOngeldigException
+            raise exception_class("Ongeldig emailadres")
+        for gebruiker in self._gebruikers:
+            if gebruiker.naam == naam and gebruiker.email == email:
+                raise GebruikerBestaatAlException("Gebruiker bestaat al")
+        gebruiker = Gebruiker(naam=naam, email=email)
+        self._gebruikers.append(gebruiker)
+        return gebruiker
+
+    def _is_geldig_email(self, email):
+        regex = r"^[\w\.\+\-]+@[\w]+\.[\w]{2,}$"
+        return re.match(regex, email) is not None
