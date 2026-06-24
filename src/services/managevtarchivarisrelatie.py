557f2b3911935d5ccb92d4d64d8f011f9f3b4771
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 20:03:20 +0200

    feat: eerste implementatiepoging ManageVTArchivarisRelatie

---
+++ src/services/managevtarchivarisrelatie.py
@@ -0,0 +1,43 @@
+from src.services.managevtarchivarisrelatie_exceptions import (
+    ArchivarisAlreadyExistsException,
+    ArchivarisRelationNotFoundException,
+    UserNotFoundException,
+    VernietigingstaakNotFoundException
+)
+
+class VTArchivarisRelatieService:
+    def add_archivaris(self, user_id, taak_id):
+        user = self.get_user_by_id(user_id)
+        taak = self.get_vernietigingstaak_by_id(taak_id)
+        if self.is_user_archivaris_of_taak(user_id, taak_id):
+            raise ArchivarisAlreadyExistsException()
+        self.add_archivaris_relation(user_id, taak_id)
+
+    def remove_archivaris(self, user_id, taak_id):
+        if not self.is_user_archivaris_of_taak(user_id, taak_id):
+            raise ArchivarisRelationNotFoundException()
+        self.remove_archivaris_relation(user_id, taak_id)
+
+    def get_archivarissen_for_taak(self, taak_id):
+        self.get_vernietigingstaak_by_id(taak_id)
+        return self.get_archivarissen_by_taak_id(taak_id)
+
+    # The following stub methods are to be filled in with actual database or repository logic
+
+    def get_user_by_id(self, user_id):
+        raise NotImplementedError
+
+    def get_vernietigingstaak_by_id(self, taak_id):
+        raise NotImplementedError
+
+    def is_user_archivaris_of_taak(self, user_id, taak_id):
+        raise NotImplementedError
+
+    def add_archivaris_relation(self, user_id, taak_id):
+        raise NotImplementedError
+
+    def remove_archivaris_relation(self, user_id, taak_id):
+        raise NotImplementedError
+
+    def get_archivarissen_by_taak_id(self, taak_id):
+        raise NotImplementedError
