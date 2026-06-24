17eca10bbce5231856ebea040ddc20d4ac703111
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 14:15:46 +0200

    test: unit tests voor CreateBron

--- tests/test_bronservice.py
+++ tests/test_bronservice.py
@@ -1,36 +1,52 @@
 import pytest
-from src.bronservice import BronService
-from src.exceptions import BronCreationError
+from src.bronservice import BronService, Bron
 
 @pytest.fixture
 def bron_service():
     return BronService()
 
 def test_create_bron_success(bron_service):
-    elementen = [{"ElementType": "ObjectType", "ElementID": 12}, 
-                 {"ElementType": "Attribute", "ElementID": 15}, 
-                 {"ElementType": "Attribute", "ElementID": 16}]
-    bron = bron_service.create_bron(elementen)
-    assert bron is not None
-    assert bron['ElementID'] == 12
-    assert bron['ElementType'] == 'ObjectType'
+    data = {
+        "name": "Test Bron",
+        "element_type": [{"ElementType": "ObjectType", "ElementID": 12}],
+        "attributes": [{"ElementType": "Attribute", "ElementID": 15}, {"ElementType": "Attribute", "ElementID": 16}]
+    }
+    bron = bron_service.create_bron(data)
+    assert bron.name == "Test Bron"
+    assert bron.element_type == [{"ElementType": "ObjectType", "ElementID": 12}]
+    assert bron.attributes == [{"ElementType": "Attribute", "ElementID": 15}, {"ElementType": "Attribute", "ElementID": 16}]
 
-def test_create_bron_empty_list(bron_service):
-    with pytest.raises(BronCreationError):
-        bron_service.create_bron([])
+def test_create_bron_missing_name(bron_service):
+    data = {
+        "element_type": [{"ElementType": "ObjectType", "ElementID": 12}],
+        "attributes": [{"ElementType": "Attribute", "ElementID": 15}, {"ElementType": "Attribute", "ElementID": 16}]
+    }
+    with pytest.raises(ValueError, match="Name is required"):
+        bron_service.create_bron(data)
 
 def test_create_bron_invalid_element_type(bron_service):
-    elementen = [{"ElementType": "InvalidType", "ElementID": 12}]
-    with pytest.raises(BronCreationError):
-        bron_service.create_bron(elementen)
+    data = {
+        "name": "Test Bron",
+        "element_type": [{"ElementType": "InvalidType", "ElementID": 99}],
+        "attributes": [{"ElementType": "Attribute", "ElementID": 15}, {"ElementType": "Attribute", "ElementID": 16}]
+    }
+    with pytest.raises(ValueError, match="Invalid element type"):
+        bron_service.create_bron(data)
 
-def test_create_bron_missing_element_id(bron_service):
-    elementen = [{"ElementType": "ObjectType"}]
-    with pytest.raises(BronCreationError):
-        bron_service.create_bron(elementen)
+def test_create_bron_no_attributes(bron_service):
+    data = {
+        "name": "Test Bron",
+        "element_type": [{"ElementType": "ObjectType", "ElementID": 12}],
+        "attributes": []
+    }
+    bron = bron_service.create_bron(data)
+    assert bron.attributes == []
 
-def test_create_bron_duplicate_elements(bron_service):
-    elementen = [{"ElementType": "ObjectType", "ElementID": 12}, 
-                 {"ElementType": "ObjectType", "ElementID": 12}]
-    with pytest.raises(BronCreationError):
-        bron_service.create_bron(elementen)
+def test_create_bron_duplicate_element_id(bron_service):
+    data = {
+        "name": "Test Bron",
+        "element_type": [{"ElementType": "ObjectType", "ElementID": 12}, {"ElementType": "ObjectType", "ElementID": 12}],
+        "attributes": [{"ElementType": "Attribute", "ElementID": 15}, {"ElementType": "Attribute", "ElementID": 16}]
+    }
+    with pytest.raises(ValueError, match="Duplicate ElementID found"):
+        bron_service.create_bron(data)
