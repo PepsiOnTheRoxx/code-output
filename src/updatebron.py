6c89a82c7388a04bc0e994f47ac510f6b8ad1259
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-24 17:52:11 +0200

    feat: eerste implementatiepoging UpdateBron

---
+++ src/updatebron.py
@@ -0,0 +1,32 @@
+from src.updatebron_exceptions import *
+
+class BronService:
+    def __init__(self):
+        self._bronnen = {}
+        self._locked_bronnen = set()
+
+    def update_bron(self, bron_id, nieuwe_data):
+        if bron_id not in self._bronnen:
+            raise BronNotFoundException()
+        if hasattr(self, '_locked_bronnen') and bron_id in self._locked_bronnen:
+            raise BronUpdateNotAllowedException()
+
+        bron = self._bronnen[bron_id]
+        updated_bron = bron.copy()
+
+        allowed_keys = {'naam', 'omschrijving'}
+
+        for key in nieuwe_data:
+            if key not in allowed_keys:
+                continue
+            if key == 'naam' and nieuwe_data[key] == '':
+                from src.updatebron_exceptions import InvalidBronDataException
+                raise InvalidBronDataException()
+        
+        if 'naam' in nieuwe_data:
+            updated_bron['naam'] = nieuwe_data['naam']
+        if 'omschrijving' in nieuwe_data:
+            updated_bron['omschrijving'] = nieuwe_data['omschrijving']
+
+        self._bronnen[bron_id] = updated_bron
+        return updated_bron
