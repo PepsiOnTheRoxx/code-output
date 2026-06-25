3e186c586f0867760849afbc283678fa6d3081dc
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-25 21:18:24 +0200

    fix: integration check poging 1

--- src/frontend/catalogusfrontend.py
+++ src/frontend/catalogusfrontend.py
@@ -12,14 +12,14 @@ def get_db():
 @catalogusfrontend_bp.route('/')
 def catalogus_frontend():
     conn = get_db()
-    boeken = conn.execute('SELECT * FROM boeken').fetchall()
+    boeken = conn.execute('SELECT rowid as id, * FROM boeken').fetchall()
     conn.close()
     return render_template('catalogus_frontend.html', boeken=boeken)
 
 @catalogusfrontend_bp.route('/boek/<int:boek_id>')
 def boek_detail_frontend(boek_id):
     conn = get_db()
-    boek = conn.execute('SELECT * FROM boeken WHERE id=?', (boek_id,)).fetchone()
+    boek = conn.execute('SELECT rowid as id, * FROM boeken WHERE rowid=?', (boek_id,)).fetchone()
     conn.close()
     return render_template('boek_detail_frontend.html', boek=boek)
 
@@ -28,8 +28,9 @@ def boek_toevoegen_frontend():
     if request.method == 'POST':
         titel = request.form['titel']
         auteur = request.form['auteur']
+        # Vul overige verplichte kolommen met None of een default
         conn = get_db()
-        conn.execute('INSERT INTO boeken (titel, auteur) VALUES (?, ?)', (titel, auteur))
+        conn.execute('INSERT INTO boeken (titel, auteur, beschrijving, is_uitgeleend, isbn, kaft_foto_url, publicatiedatum, uitgeleend_datum, uitgeleend_max_tot) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', (titel, auteur, None, None, None, None, None, None, None))
         conn.commit()
         boek_id = conn.execute('SELECT last_insert_rowid()').fetchone()[0]
         conn.close()
@@ -42,10 +43,10 @@ def boek_aanpassen_frontend(boek_id):
     if request.method == 'POST':
         titel = request.form['titel']
         auteur = request.form['auteur']
-        conn.execute('UPDATE boeken SET titel=?, auteur=? WHERE id=?', (titel, auteur, boek_id))
+        conn.execute('UPDATE boeken SET titel=?, auteur=? WHERE rowid=?', (titel, auteur, boek_id))
         conn.commit()
         conn.close()
         return redirect(url_for('catalogusfrontend.boek_detail_frontend', boek_id=boek_id))
-    boek = conn.execute('SELECT * FROM boeken WHERE id=?', (boek_id,)).fetchone()
+    boek = conn.execute('SELECT rowid as id, * FROM boeken WHERE rowid=?', (boek_id,)).fetchone()
     conn.close()
     return render_template('boek_aanpassen_frontend.html', boek=boek)
