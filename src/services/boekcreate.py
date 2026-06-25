b2d68ace4aa7c64050451d5f13be5658219f3806
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-25 16:38:17 +0200

    feat: eerste implementatiepoging BoekCreate

---
+++ src/services/boekcreate.py
@@ -0,0 +1,41 @@
+from src.services.boekcreate_exceptions import BoekAlreadyExistsException, BoekCreateFailedException
+
+class BoekService:
+    def __init__(self, db_connection):
+        self.db_connection = db_connection
+
+    def create(self, boek_data):
+        cursor = self.db_connection.cursor()
+        try:
+            cursor.execute(
+                "SELECT 1 FROM boek WHERE isbn = %s",
+                (boek_data['isbn'],)
+            )
+            if cursor.fetchone() is not None:
+                raise BoekAlreadyExistsException()
+
+            cursor.execute(
+                "INSERT INTO boek (titel, auteur, isbn, publicatiejaar, uitgeverij, pagina_count, genre, taal, samenvatting)"
+                " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                (
+                    boek_data['titel'],
+                    boek_data['auteur'],
+                    boek_data['isbn'],
+                    boek_data['publicatiejaar'],
+                    boek_data['uitgeverij'],
+                    boek_data['pagina_count'],
+                    boek_data['genre'],
+                    boek_data['taal'],
+                    boek_data['samenvatting'],
+                )
+            )
+            if cursor.rowcount == 0:
+                raise BoekCreateFailedException()
+            self.db_connection.commit()
+            return True
+        except Exception as e:
+            try:
+                self.db_connection.rollback()
+            except Exception:
+                pass
+            raise BoekCreateFailedException() from e
