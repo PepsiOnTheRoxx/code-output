3a825a043fc33fadbdb0d25533a9f92822fe01cb
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-25 15:16:42 +0200

    test: exceptions + tests voor BoekUpdate

--- src/services/boekupdate_exceptions.py
+++ src/services/boekupdate_exceptions.py
@@ -1,19 +1,19 @@
 class BoekUpdateException(Exception):
-    """Basis-exceptie voor BoekUpdate-feature."""
+    """Basisexception voor alle BoekUpdate gerelateerde fouten."""
     pass
 
 class BoekNietGevondenException(BoekUpdateException):
-    """Boek met opgegeven ID is niet gevonden."""
+    """Exception wanneer het boek niet gevonden wordt in de database."""
     pass
 
 class OngeldigeBoekDataException(BoekUpdateException):
-    """De aangeleverde boekdata is ongeldig."""
+    """Exception voor ongeldige of incomplete boekdata tijdens updaten."""
     pass
 
 class DatabaseUpdateFoutException(BoekUpdateException):
-    """Er is een fout opgetreden tijdens het bijwerken van de database."""
+    """Exception bij fouten tijdens het wegschrijven van de update in de database."""
     pass
 
 class GeenWijzigingenGedetecteerdException(BoekUpdateException):
-    """Er zijn geen wijzigingen om op te slaan."""
+    """Exception wanneer er geen wijzigingen zijn tussen oude en nieuwe boekdata."""
     pass
