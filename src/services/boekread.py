b69b52989e44dca6d826e07076c28ae9dcccb492
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-26 11:10:38 +0200

    fix: integration check poging 5

--- src/services/boekread.py
+++ src/services/boekread.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from database import get_connection
 from .boekread_exceptions import BoekNotFoundException, InvalidBoekIdException
 
