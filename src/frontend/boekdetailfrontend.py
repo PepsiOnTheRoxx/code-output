6f5adcbfc74734044687e7a776f9b264ffca352b
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-27 13:34:11 +0200

    feat: implementatie van BoekDetailFrontend

--- src/frontend/boekdetailfrontend.py
+++ src/frontend/boekdetailfrontend.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, abort
+from flask import Blueprint, render_template, redirect, url_for, request
 import sqlite3
 from database import get_connection, DB_PATH
 
@@ -12,8 +12,8 @@ def get_db():
 @boekdetailfrontend_bp.route('/boek/<int:boek_id>')
 def detail(boek_id):
     conn = get_db()
-    boek = conn.execute('SELECT rowid as id, auteur, beschrijving, isbn, publicatiedatum, kaft_foto_url, is_uitgeleend, uitgeleend_datum, uitgeleend_max_tot FROM boeken WHERE rowid = ?', (boek_id,)).fetchone()
+    boek = conn.execute('SELECT rowid as id, auteur, beschrijving, isbn, publicatiedatum, kaft_foto_url, is_uitgeleend, uitgeleend_datum, uitgeleend_max_tot FROM boeken WHERE rowid=?', (boek_id,)).fetchone()
     conn.close()
-    if boek is None:
-        abort(404)
-    return render_template('boek_detail.html', boek=boek)
+    if not boek:
+        return render_template('boekdetailfrontend_nietgevonden.html'), 404
+    return render_template('boekdetailfrontend_detail.html', boek=boek)
