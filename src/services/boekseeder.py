f4d5115c8f80eb7e0517ccd3405e2df390eaddf9
pepsi-on-the-rox <94600670+pepsi-on-the-rox@users.noreply.github.com>
2026-06-27 13:32:53 +0200

    fix: integration check poging 1

--- src/services/boekseeder.py
+++ src/services/boekseeder.py
@@ -12,6 +12,7 @@ class BoekSeeder:
             {
                 'auteur': "Arthur Conan Doyle",
                 'beschrijving': "Een klassiek mysterieboek met detective Sherlock Holmes.",
+                'titel': "Sherlock Holmes Verhalenbundel",  # Toegevoegd
                 'isbn': "9789012345678",
                 'publicatiedatum': "1892-10-14",
                 'kaft_foto_url': "http://dummy.url/holmes.jpg",
@@ -22,6 +23,7 @@ class BoekSeeder:
             {
                 'auteur': "J.K. Rowling",
                 'beschrijving': "Het eerste deel in de magische Harry Potter-serie.",
+                'titel': "Harry Potter en de Steen der Wijzen",  # Toegevoegd
                 'isbn': "9780747532743",
                 'publicatiedatum': "1997-06-26",
                 'kaft_foto_url': "http://dummy.url/hp1.jpg",
@@ -32,6 +34,7 @@ class BoekSeeder:
             {
                 'auteur': "George Orwell",
                 'beschrijving': "Dystopische roman over een totalitaire samenleving.",
+                'titel': "1984",  # Toegevoegd
                 'isbn': "9780451524935",
                 'publicatiedatum': "1949-06-08",
                 'kaft_foto_url': "http://dummy.url/1984.jpg",
@@ -42,6 +45,7 @@ class BoekSeeder:
             {
                 'auteur': "Harper Lee",
                 'beschrijving': "Een diepgaand verhaal over racisme en onschuld.",
+                'titel': "To Kill a Mockingbird",  # Toegevoegd
                 'isbn': "9780061120084",
                 'publicatiedatum': "1960-07-11",
                 'kaft_foto_url': "http://dummy.url/mockingbird.jpg",
@@ -52,6 +56,7 @@ class BoekSeeder:
             {
                 'auteur': "F. Scott Fitzgerald",
                 'beschrijving': "Amerikaans meesterwerk over The Jazz Age.",
+                'titel': "The Great Gatsby",  # Toegevoegd
                 'isbn': "9780743273565",
                 'publicatiedatum': "1925-04-10",
                 'kaft_foto_url': "http://dummy.url/gatsby.jpg",
